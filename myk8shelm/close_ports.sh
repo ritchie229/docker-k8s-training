cdd19e9c516e154b1656ba218b7a6bbc905bce74
ritchie229 <rishat.mukhtarov@gmail.com>
2025-12-02 00:55:40 +0300

    Modified scripts, Svctypr NodePort in values, taskfile - trying argocd

--- myk8shelm/close_ports.sh
+++ myk8shelm/close_ports.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
-#echo "=================LOCKING IP FORWARDING====================="
-#iptables -C DOCKER-USER -i ens160 -m conntrack --ctstate DNAT -j ACCEPT 2>/dev/null && \
-#iptables -D DOCKER-USER -i ens160 -m conntrack --ctstate DNAT -j ACCEPT
 
+read -p "Pls specify the ServiceName: " SERVICENAME
+read -p "Pls specify the NameSpace: " NAMESPACE
+read -p "Pls specify the a Port for external access: " EXT_PORT
 
-#read -p "Enter NS: " namespace
-
-#sysctl -w net.ipv4.ip_forward=0
-#sysctl -w net.ipv4.conf.all.route_localnet=0
-
-namespace=$(kubectl get ns | grep -oE '\b(prod|dev)\b' | head -n1)
-PORT=$(kubectl describe svc -n $namespace | grep NodePort | awk '{print $3}' | awk -F'/' '{printf $1}')
+PORT=$(kubectl get svc -n $NAMESPACE $SERVICENAME -o jsonpath='{.spec.ports[0].nodePort}')
 CLUSTER_IP=$(minikube ip --profile minihelm)
+
 echo "===========REMOVING IP FORWARDING MAIN RULES==============="
-iptables -t nat -D PREROUTING -p tcp --dport 30001 -j DNAT --to-destination $CLUSTER_IP:$PORT
-#iptables -t nat -D OUTPUT -p tcp --dport 30001 -j DNAT --to-destination $CLUSTER_IP:$PORT
-#iptables -t nat -D POSTROUTING -d $CLUSTER_IP -p tcp --dport $PORT -j MASQUERADE
-#iptables -t nat -D POSTROUTING -j MASQUERADE
+iptables -t nat -D PREROUTING -p tcp --dport $EXT_PORT -j DNAT --to-destination $CLUSTER_IP:$PORT
 
-
-#echo "=============DOCKER UNLOCKED IP FORWARDING================="
-#iptables -L DOCKER-USER -nv --line-numbers
 echo "==============$CLUSTER_IP:$PORT CLOSED!!!=================="
 iptables -t nat -L PREROUTING -n --line-numbers
-#iptables -t nat -L OUTPUT -n --line-numbers
-#iptables -t nat -L POSTROUTING -n --line-numbers
-
-
 
--- myk8shelm/open_ports.sh
+++ myk8shelm/open_ports.sh
@@ -4,20 +4,28 @@ iptables -C DOCKER-USER -i ens160 -m conntrack --ctstate DNAT -j ACCEPT 2>/dev/n
 iptables -I DOCKER-USER -i ens160 -m conntrack --ctstate DNAT -j ACCEPT
 
 
-#read -p "Pls specify the NameSpace: " namespace
 echo "=================ENABLE IP FORWARDING=============="
 sysctl -w net.ipv4.ip_forward=1
 #sysctl -w net.ipv4.conf.all.route_localnet=1
 
+read -p "Pls specify the ServiceName: " SERVICENAME
+read -p "Pls specify the NameSpace: " NAMESPACE
+read -p "Pls specify the a Port for external access: " EXT_PORT
+#NAMESPACE=$(kubectl get ns | grep -oE '\b(prod|dev)\b' | head -n1)
 
-namespace=$(kubectl get ns | grep -oE '\b(prod|dev)\b' | head -n1)
-PORT=$(kubectl describe svc -n $namespace | grep NodePort | awk '{print $3}' | awk -F'/' '{printf $1}')
-CLUSTER_IP=$(minikube ip --profile minihelm)
-echo "=================ADD 30001 RULE===================="
-iptables -t nat -A PREROUTING -p tcp --dport 30001 -j DNAT --to-destination $CLUSTER_IP:$PORT
-#iptables -t nat -A OUTPUT -p tcp --dport 30001 -j DNAT --to-destination $CLUSTER_IP:$PORT
+#PORT=$(kubectl describe svc -n $NAMESPACE | grep NodePort | awk '{print $3}' | awk -F'/' '{printf $1}')
+PORT=$(kubectl get svc -n $NAMESPACE $SERVICENAME -o jsonpath='{.spec.ports[0].nodePort}')
+
+
+MINI_PROFILE=$(minikube profile | awk '{print $2}')
+CLUSTER_IP=$(minikube ip --profile $MINI_PROFILE)
+echo "==========ADD EXTERNAL PORT RULE For Argo CD =============="
+iptables -t nat -A PREROUTING -p tcp --dport $EXT_PORT -j DNAT --to-destination $CLUSTER_IP:$PORT
+
+#iptables -t nat -A OUTPUT -p tcp --dport $EXT_PORT -j DNAT --to-destination $CLUSTER_IP:$PORT
 #iptables -t nat -A POSTROUTING -d $CLUSTER_IP -p tcp --dport $PORT -j MASQUERADE
 #iptables -t nat -A POSTROUTING -j MASQUERADE
+
 
 echo "===========DOCKER UNLOCKED IP FORWARDING==========="
 echo "+++++++++++++++++++++see+++++++++++++++++++++++++++"
